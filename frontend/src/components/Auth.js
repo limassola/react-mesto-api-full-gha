a4b5f1b7f2cbc70eee0a15504a35fa74ff20c0ad
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-07-08 18:56:56 +0300

    fix: изменение базового роута на домен бэка

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://limassola.nomoreparties.sbs';
+export const BASE_URL = 'https://api.limassola.nomoreparties.sbs';
 
 export const signup = (password, email) => {
     return fetch(`${BASE_URL}/signup`, {
