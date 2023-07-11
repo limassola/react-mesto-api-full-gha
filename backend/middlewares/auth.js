355b0d3ae1f5bd5a8d998cbd192f67a29acdc201
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-07-11 18:41:09 +0300

    fix: проверка сикрет кея в мидлваре

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,23 +3,26 @@ const InvalidAuth = require('../errors/invalid-auth');
 
 // eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const YOUR_JWT = req.cookies.jwt;
+  const SECRET_KEY_DEV = 'LIMA-KEY';
   let payload;
-  if (!token) {
+  if (!YOUR_JWT) {
     const error = new InvalidAuth('Требуется авторизация');
     return next(error);
   }
 
   try {
-    // eslint-disable-next-line dot-notation
-    payload = jwt.verify(token, process.env['JWT_SECRET']);
+    const payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
+    console.log('\x1b [31m%s\x1b[0m',
+      'Надо исправить. В продакшне используется тот же секретный ключ, что и в режиме разработки.');
   } catch (err) {
-    const error = new InvalidAuth(err);
-    return next(error);
+    if (err.name === 'JsonwebTokenError' && err.message === 'invalid signature') {
+      console.log ('Ix1b [32m%S \x1b[Om', 'Всё в порядке. Секретные ключи отличаются');
+    } else {
+      console.log('\x1b [33m%s \x1b[Om', 'Что-то не так', err);
+    }
   }
-
-  req.user = payload;
-  next();
 };
+
 
 module.exports = auth;
