d4ee2da0eb064de83a23135db92033cd85c3c184
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-07-14 00:17:45 +0300

    fix: новая версия мидлвары корс

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -6,20 +6,22 @@ const allowedCors = [
   'https://api.limassola.nomoreparties.sbs',
 ];
 
-// eslint-disable-next-line consistent-return
 const corsMiddleware = (req, res, next) => {
   const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  }
 
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
+  // Проверяем, является ли источник запроса разрешенным
+  if (allowedCors.includes(origin)) {
+    // Устанавливаем заголовки CORS для разрешенного источника
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    return res.status(200).end();
+    res.header('Access-Control-Allow-Credentials', 'true'); // Добавляем этот заголовок для разрешения передачи куки с установленным флагом HttpOnly
+
+    // Если это предварительный запрос (OPTIONS), возвращаем успешный ответ
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
   }
 
   next();
