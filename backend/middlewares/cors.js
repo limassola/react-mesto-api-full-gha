d97faafd5913a52f59dcbb301cfcbf2b02d65e64
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-07-13 23:50:07 +0300

    fix: обновление корс

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,13 +9,14 @@ const corsMiddleware = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   }
 
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers');
     return res.status(200).end();
   }
 
