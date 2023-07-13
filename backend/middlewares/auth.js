516ab814cab23181330662d796333cc348049831
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-07-14 00:39:06 +0300

    fix: возвращена старая мидлвара auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,34 +1,26 @@
-/* eslint-disable func-call-spacing */
-/* eslint-disable no-spaced-func */
-/* eslint-disable no-console */
-/* eslint-disable no-unused-vars */
-/* eslint-disable function-paren-newline */
-/* eslint-disable no-shadow */
 const jwt = require('jsonwebtoken');
 const InvalidAuth = require('../errors/invalid-auth');
 require('dotenv').config();
 
 // eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
-  const YOUR_JWT = req.cookies.jwt;
-  const { SECRET_KEY_DEV } = process.env;
+  const token = req.cookies.jwt;
   let payload;
-  if (!YOUR_JWT) {
+  if (!token) {
     const error = new InvalidAuth('Требуется авторизация');
     return next(error);
   }
 
   try {
-    payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
-    console.log('\x1b [31m%s\x1b[0m',
-      'Надо исправить. В продакшне используется тот же секретный ключ, что и в режиме разработки.');
+    // eslint-disable-next-line dot-notation
+    payload = jwt.verify(token, process.env['JWT_SECRET']);
   } catch (err) {
-    if (err.name === 'JsonwebTokenError' && err.message === 'invalid signature') {
-      console.log ('Ix1b [32m%S \x1b[Om', 'Всё в порядке. Секретные ключи отличаются');
-    } else {
-      console.log('\x1b [33m%s \x1b[Om', 'Что-то не так', err);
-    }
+    const error = new InvalidAuth(err);
+    return next(error);
   }
+
+  req.user = payload;
+  next();
 };
 
 module.exports = auth;
